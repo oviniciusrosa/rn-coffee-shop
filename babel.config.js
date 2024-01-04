91a7c96af5b6b5c4382c1fc6a1fa0b510a6fcb52
oviniciusrosa <bakavini99@gmail.com>
2024-01-04 13:54:36 -0300

    chore: setup absolute path

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,31 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel", "react-native-reanimated/plugin"],
+    plugins: [
+      "expo-router/babel",
+
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "react-native-sqlite-storage": "react-native-quick-sqlite",
+            "~/assets": "./src/assets/",
+            "~/contexts": "./src/contexts/",
+            "~/components": "./src/components/",
+            "~/constants": "./src/constants/",
+            "~/external": "./src/external/",
+            "~/features": "./src/features/",
+            "~/store": "./src/store/",
+            "~/services": "./src/services/",
+            "~/theme": "./src/theme/",
+            "~/hooks": "./src/hooks/",
+            "~/utils": "./src/utils/",
+          },
+        },
+      ],
+
+      "react-native-reanimated/plugin",
+    ],
   };
 };
